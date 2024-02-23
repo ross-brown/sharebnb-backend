4fbb0140d622c66e2e377c8113fbf35b33ff7314
ross-brown <rbrown_98@yahoo.com>
2024-02-23 14:04:00 -0600

    finish POST and PATCH tests for users routes

--- routes/users.test.js
+++ routes/users.test.js
@@ -157,11 +157,132 @@ describe("GET /users/:username/sent", function () {
   });
 });
 
+describe("POST /users", function () {
+  const newUser = {
+    username: "newUser",
+    firstName: "new",
+    lastName: "user",
+    email: "new@email.com"
+  };
+  test("works", async function () {
+    const resp = await request(app)
+      .post("/users")
+      .send({
+        ...newUser,
+        password: "password"
+      });
 
-// works with correct request body (check for 201)
-// bad request with incorrect/missing data
-// bad request with duplicate username
+    expect(resp.body).toEqual({
+      user: newUser,
+      token: expect.any(String)
+    });
+    expect(resp.statusCode).toEqual(201);
+  });
 
-// describe("POST /users", function () {
+  test("bad request with missing data", async function () {
+    const resp = await request(app)
+      .post("/users")
+      .send({
+        username: "user",
+        password: "password",
+        firstName: "new"
+      });
 
+    expect(resp.statusCode).toEqual(400);
+  });
+
+  test("bad request with incorrect data", async function () {
+    const resp = await request(app)
+      .post("/users")
+      .send({
+        ...newUser,
+        isSuperUser: true
+      });
+
+    expect(resp.statusCode).toEqual(400);
+  });
+
+  test("bad request with duplicate username", async function () {
+    const resp = await request(app)
+      .post("/users")
+      .send({
+        username: "u1",
+        password: "password",
+        firstName: "new",
+        lastName: "user"
+      });
+
+    expect(resp.statusCode).toEqual(400);
+  });
+});
+
+describe("PATCH /users/:username", function () {
+  test("works with correct data", async function () {
+    const resp = await request(app)
+      .patch("/users/u1")
+      .set("authorization", `Bearer ${u1Token}`)
+      .send({
+        lastName: "newLastName",
+        email: "newEmail@email.com"
+      });
+
+    expect(resp.body).toEqual({
+      user: {
+        username: "u1",
+        firstName: "U1F",
+        lastName: "newLastName",
+        email: "newEmail@email.com"
+      }
+    });
+  });
+
+  test("bad request with incorrect data", async function () {
+    const resp = await request(app)
+      .patch("/users/u1")
+      .set("authorization", `Bearer ${u1Token}`)
+      .send({
+        username: "newName",
+        isAdmin: true
+      });
+
+    expect(resp.statusCode).toEqual(400);
+  });
+
+  test("unauth if wrong user", async function () {
+    const resp = await request(app)
+      .patch("/users/u1")
+      .set("authorization", `Bearer ${u2Token}`)
+      .send({
+        firstName: "HAHAHAH"
+      });
+
+    expect(resp.statusCode).toEqual(401);
+  });
+
+  test("unauth if anon", async function () {
+    const resp = await request(app)
+      .patch("/users/u1")
+      .send({
+        firstName: "notMe"
+      });
+
+    expect(resp.statusCode).toEqual(401);
+  });
+
+
+});
+
+
+//works with correct user
+//unauth for wrong user
+//unauth for anon
+
+// describe("DELETE /users/:username", function () {
+//   test("works with correct user", async function () {
+//     const resp = await request(app)
+//       .delete("/users/u1")
+//       .set("authorization", `Bearer ${u1Token}`);
+
+//     expect(resp.body).toEqual({ deleted: "u1" });
+//   });
 // });
